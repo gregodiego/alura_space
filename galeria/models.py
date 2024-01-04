3ee728ae8be0004d10c1c4cb2a2e3b46907a636d
gregodiego <gregodiego@hotmail.com>
2024-01-04 13:02:36 -0300

    update no model

--- galeria/models.py
+++ galeria/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, blank=False, null=False, choices=OPCOES_CATEGORIA, default='')
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f'fotografia [nome={self.nome}]'
